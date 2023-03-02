ce1743bdd6b3c10fa3ec13eab98fcf733419bab7
Cem Sertkaya <cemsertkaya99@gmail.com>
2023-03-02 13:36:11 +0300

    Feature: Icon colors are implemented

--- Signal/src/ViewControllers/HomeView/HomeTabBarController.swift
+++ Signal/src/ViewControllers/HomeView/HomeTabBarController.swift
@@ -17,13 +17,13 @@ class HomeTabBarController: UITabBarController {
     lazy var settingsTabBarItem = UITabBarItem(title: NSLocalizedString("SETTINGS_NAV_BAR_TITLE",
         comment: "Title for settings activity"),
         image: UIImage(named: "settings-tab-bar"),
-        selectedImage: UIImage(named: "selected_settings_tab_bar"))
+        selectedImage: UIImage(named: "selected_settings_tab_bar")!.tintedImage(color: UIColor(red: 0.24, green: 0.55, blue: 1.00, alpha: 1.00)))
     lazy var chatListViewController = ChatListViewController()
     lazy var chatListNavController = OWSNavigationController(rootViewController: chatListViewController)
     lazy var chatListTabBarItem = UITabBarItem(
         title: NSLocalizedString("CHAT_LIST_TITLE_INBOX", comment: "Title for the chat list's default mode."),
         image: UIImage(named: "chats-tab-bar"),
-        selectedImage: UIImage(named: "selected-chats-tab-bar")
+        selectedImage: UIImage(named: "selected-chats-tab-bar")!.tintedImage(color: UIColor(red: 0.24, green: 0.55, blue: 1.00, alpha: 1.00))
     )
 
     lazy var storiesViewController = StoriesViewController()
@@ -92,7 +92,7 @@ class HomeTabBarController: UITabBarController {
 
     @objc
     func applyTheme() {
-        tabBar.tintColor = UIColor(red: 0.173, green: 0.717, blue: 0.423, alpha: 1)
+        tabBar.tintColor = UIColor(red: 0.24, green: 0.55, blue: 1.00, alpha: 1.00)
     }
 
     @objc
