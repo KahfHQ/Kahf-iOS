3ae69736fbd5304218876f173dddbd5350edb912
Cem Sertkaya <cemsertkaya99@gmail.com>
2024-01-04 21:13:33 +0300

    Kahf-iOS #18 Home page design#18

--- Signal/src/ViewControllers/Home/HomeVC.swift
+++ Signal/src/ViewControllers/Home/HomeVC.swift
@@ -82,6 +82,8 @@ class HomeVC: UITableViewController {
     }
     
     func fetchTimes(coordinate: CLLocationCoordinate2D) {
+        self.contents.removeAll()
+        self.contents.append(.time(date: Date()))
         getAddressFromCoordinates(latitude: coordinate.latitude, longitude: coordinate.longitude) { city in
             self.prayerManager.getCurrentNextPrayerTimes(coordinate: coordinate, completion: { current, next, countdown in
                 guard let next = next, let countdown = countdown else { return }
